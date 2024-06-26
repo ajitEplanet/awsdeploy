c16d8df53acc92b70de8e253d290c41297ab1198
ajitEplanet <ajit_mohapatra@eplanetsoft.com>
2024-06-26 21:39:45 +0530

    update user

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const port = 8000;
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+app.get('/getUser', (req, res) => {
+  res.send({username:'ajit',userid:'11:11',usermobile:'59976131489',userAdrees:'odisha',});
+});
 console.log('hihello hi fff')
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
