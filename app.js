4be7152f74d20d497078040ac2236793fada0085
ajitEplanet <ajit_mohapatra@eplanetsoft.com>
2024-06-26 19:18:47 +0530

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8000;
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
-console.log('hihello fff')
+console.log('hihello hi fff')
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
 });
